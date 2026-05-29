8ee5923275de2f6bc8f12823c9c7c11cbae568ce
DanielAsDano <daniel.cardenas1767@gmail.com>
2026-05-29 16:03:22 -0600

    añadir metodo obtenerProductoPorId al controller

--- src/main/java/dc/sistemadeinventarios/controller/ProductoController.java
+++ src/main/java/dc/sistemadeinventarios/controller/ProductoController.java
@@ -1,10 +1,12 @@
 package dc.sistemadeinventarios.controller;
 
+import dc.sistemadeinventarios.exception.RecursoNoEncontrado;
 import dc.sistemadeinventarios.model.Producto;
 import dc.sistemadeinventarios.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,5 +33,16 @@ public class ProductoController {
         logger.info("Producto agregado: " + producto);
         productoService.guardarProducto(producto);
         return producto;
+    }
+
+    @GetMapping("/productos/{id}")
+    public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable Integer id){
+        Producto producto = productoService.buscarProductoPorId(id);
+        logger.info("Producto encontrado: " + producto);
+        if (producto != null){
+            return ResponseEntity.ok(producto);
+        }else {
+            throw new RecursoNoEncontrado("no se encontro el producto con el id " + id);
+        }
     }
 }
